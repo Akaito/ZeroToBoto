50c6619b185685e90048d0fa1b7cf78965952deb
Chris Barrett <chris@codesaru.com>
2017-07-11 22:57:06 -0700

    Completed first exercise.

--- assets/hangman-0.py
+++ assets/hangman-0.py
@@ -1,16 +1,19 @@
 # hangman-0.py
 
-def partial_word(word):
+def partial_word(word, revealed):
     """Returns the target word, each character separated by spaces, with
     letters not yet guessed shown as underscores.
     """
     # gradually build the revealed/hidden parts of the word
     word_so_far = ''
-    for _ in word:
+    for letter in word:
         # write either the current letter of the target word, or a
         # period, depending on if the user has guessed the letter
         # yet or not
-        word_so_far = word_so_far + '.'
+        if revealed:
+            word_so_far = word_so_far + letter
+        else:
+            word_so_far = word_so_far + '.'
     return word_so_far
 
 
@@ -18,5 +21,6 @@ def partial_word(word):
 # the word that must be guessed
 target_word = 'otter'
 
-print 'Word so far: ' + partial_word(target_word)
+print 'Word so far: ' + partial_word(target_word, True)
+print 'Word so far: ' + partial_word(target_word, False)
 
